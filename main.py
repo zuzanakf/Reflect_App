3cd4cf59ba8be76880e0020f20e8aa69527c3fea
zuzanakf <zuzana.fernandes@yahoo.co.uk>
2023-08-19 22:03:50 +0100

    capital

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.markdown(
         display: none
     }
     [data-testid="stAppViewContainer"] {
-        background: linear-gradient(#A9D6E4, #eff9e7)
+        background: linear-gradient(#a9d6e4, #eff9e7)
     }
 </style>
 """,
