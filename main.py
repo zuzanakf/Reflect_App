2f2b434b4467f1478ea27740b56147d59976851a
zuzanakf <zuzana.fernandes@yahoo.co.uk>
2023-08-19 22:04:58 +0100

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.markdown(
         display: none
     }
     [data-testid="stAppViewContainer"] {
-        background: linear-gradient(#a9d6e4, #eff9e7)
+        background: linear-gradient(#a9d6e4, #7B26E0)
     }
 </style>
 """,
